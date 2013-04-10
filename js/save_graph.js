078f337669062215d02950391feca5a118e6f5ac
nmorse <morse.nate@gmail.com>
2013-04-10 08:57:53 -0400

    storage and delete

--- js/save_graph.js
+++ js/save_graph.js
@@ -1,12 +1,10 @@
 $(function() {
     $(document).on("save_hbg", function (event, g, overwrite, online_service) {
-        alert("click");
         var outcome = ["storage not availible", "saved to existing", "did not overwrite existing", "created new", "storage operation submitted"];
         var proposed_name = g.graph.name;
         var local_hb_graphs;
         if (typeof Storage !== "undefined") {
             //use local storage
-            alert("use local storage");
             if (!localStorage.hb_graphs) {
                 localStorage.hb_graphs = "{}";
             }
@@ -15,45 +13,107 @@ $(function() {
                 if (overwrite) {
                     local_hb_graphs[proposed_name] = JSON.parse(export_graph_json(g));
                     localStorage.hb_graphs = JSON.stringify(local_hb_graphs);
-                    $(document).trigger("hbg_save_status", [{"outcome": outcome[1], "dest": "local", "final":true}]);
+                    $(document).trigger("hbg_save_status", [{"outcome": outcome[1], "target": "local", "final":true}]);
                 }
                 else {
-                    $(document).trigger("hbg_save_status", [{"outcome": outcome[2], "dest": "local", "final":true}]);
+                    $(document).trigger("hbg_save_status", [{"outcome": outcome[2], "target": "local", "final":true}]);
                 }
             }
             else {
                 local_hb_graphs[proposed_name] = JSON.parse(export_graph_json(g));
                 localStorage.hb_graphs = JSON.stringify(local_hb_graphs);
-                $(document).trigger("hbg_save_status", [{"outcome": outcome[3], "dest": "local", "final":true}]);
+                $(document).trigger("hbg_save_status", [{"outcome": outcome[3], "target": "local", "final":true}]);
             }
         }
         else {
-            $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "dest": "local", "final":true}]);
+            $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "target": "local", "final":true}]);
         }
         
         if (navigator.online) {
             if (online_service) {
                 //post to service
-                $(document).trigger("hbg_save_status", [{"outcome": outcome[4], "dest": "online", "final":false}]);
+                $(document).trigger("hbg_save_status", [{"outcome": outcome[4], "target": "online", "final":false}]);
             }
             else {
-                $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "dest": "online", "final":true}]);
+                $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
             }
         }
         else {
-            $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "dest": "online", "final":true}]);
+            $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
         }
     });
     
     $(document).on("hbg_save_status", function(event, arg) {
-        alert(arg.outcome+" "+arg.dest+" is_final:"+arg.final);
+        alert(arg.outcome+" "+arg.target+" is_final:"+arg.final);
     });
     
-    $('#to_storage').on("click", function(event) {
+    $('#save_to_storage').on("click", function(event) {
         var overwrite = false;
         var online_service = null;
-        g.graph = {};
-        g.graph.name = "test";
+        if (!g.graph) {g.graph = {};}
+        g.graph.name = $('#graph_input_name').val();
         $(document).trigger("save_hbg", [g, overwrite, online_service]);
     });
+    
+    $(document).on("delete_hbg", function (event, path_name, online_service) {
+        var outcome = ["storage not availible", "deleted", "graph not found", "error during delete request", "delete operation submitted"];
+        var local_hb_graphs;
+        if (typeof Storage !== "undefined") {
+            //use local storage
+            if (!localStorage.hb_graphs) {
+                localStorage.hb_graphs = "{}";
+            }
+            local_hb_graphs = JSON.parse(localStorage.hb_graphs);
+            if(local_hb_graphs[path_name]) {
+                delete local_hb_graphs[path_name];
+                localStorage.hb_graphs = JSON.stringify(local_hb_graphs);
+                $(document).trigger("hbg_delete_status", [{"outcome": outcome[1], "target": "local", "final":true}]);
+            }
+            else {
+                $(document).trigger("hbg_delete_status", [{"outcome": outcome[2], "target": "local", "final":true}]);
+            }
+        }
+        else {
+            $(document).trigger("hbg_delete_status", [{"outcome": outcome[0], "target": "local", "final":true}]);
+        }
+        
+        if (navigator.online) {
+            if (online_service) {
+                //post to service
+                $(document).trigger("hbg_delete_status", [{"outcome": outcome[4], "target": "online", "final":false}]);
+            }
+            else {
+                $(document).trigger("hbg_delete_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
+            }
+        }
+        else {
+            $(document).trigger("hbg_delete_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
+        }
+    });
+    
+    $(document).on("hbg_delete_status", function(event, arg) {
+        alert(arg.outcome+" "+arg.target+" is_final:"+arg.final);
+    });
+    
+    $('#delete_from_storage').on("click", function(event) {
+        var path_name = $('#graph_input_name').val();
+        var online_service = null;
+        
+        $(document).trigger("delete_hbg", [path_name, online_service]);
+    });
+    
+    $('#graph_input_name').data("source", request_hbg_names()); 
+
 });
+
+function request_hbg_names () {
+    var names = [];
+    if (typeof Storage !== "undefined") {
+        //use local storage
+        if (localStorage.hb_graphs) {
+        }
+        local_hb_graphs = JSON.parse(localStorage.hb_graphs);
+        return $.map(local_hb_graphs, function(v,k){return k;});
+    }
+    return names;
+}
