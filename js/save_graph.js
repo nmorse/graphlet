08a09bd2bfc4b116a46820022d2e893f6b294c17
nmorse <morse.nate@gmail.com>
2013-04-15 21:14:10 -0400

    load pubsub calls

--- js/save_graph.js
+++ js/save_graph.js
@@ -1,4 +1,54 @@
 $(function() {
+    // load
+    $(document).on("load_hbg", function (event, g, online_service) {
+        var outcome = ["storage not availible", "loaded", "not found", "load operation submitted"];
+        var proposed_name = g.graph.name;
+        var local_hb_graphs;
+        if (typeof Storage !== "undefined") {
+            //use local storage
+            if (!localStorage.hb_graphs) {
+                localStorage.hb_graphs = "{}";
+            }
+            local_hb_graphs = JSON.parse(localStorage.hb_graphs);
+            if(local_hb_graphs[proposed_name]) {
+                g = local_hb_graphs[proposed_name];
+                $(document).trigger("hbg_load_status", [{"outcome": outcome[1], "target": "local", "final":true}]);
+            }
+            else {
+                $(document).trigger("hbg_load_status", [{"outcome": outcome[2], "target": "local", "final":true}]);
+            }
+        }
+        else {
+            $(document).trigger("hbg_load_status", [{"outcome": outcome[0], "target": "local", "final":true}]);
+        }
+        
+        if (navigator.online) {
+            if (online_service) {
+                //get from service
+                $(document).trigger("hbg_load_status", [{"outcome": outcome[3], "target": "online", "final":false}]);
+            }
+            else {
+                $(document).trigger("hbg_load_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
+            }
+        }
+        else {
+            $(document).trigger("hbg_load_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
+        }
+    });
+    
+    $(document).on("hbg_load_status", function(event, arg) {
+        alert(arg.outcome+" "+arg.target+" is_final:"+arg.final);
+    });
+    
+    $('#load_from_storage').on("click", function(event) {
+        var overwrite = false;
+        var online_service = null;
+        if (!g.graph) {g.graph = {};}
+        g.graph.name = $('#graph_input_name').val();
+        $(document).trigger("load_hbg", [g, overwrite, online_service]);
+    });
+    
+    // save
     $(document).on("save_hbg", function (event, g, overwrite, online_service) {
         var outcome = ["storage not availible", "saved to existing", "did not overwrite existing", "created new", "storage operation submitted"];
         var proposed_name = g.graph.name;
