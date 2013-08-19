0093d919c49acbc1a76db92ce27335ac3463a746
nmorse <morse.nate@gmail.com>
2013-08-18 21:12:33 -0400

    first stab at run in the browser not working yet

---
+++ js/honeybee_graph_run.js
@@ -0,0 +1,75 @@
+// honeybee graph run
+// 
+
+$(function() {
+
+    init_run = function(hbg) {
+        var listeners = this.graph_selector(hbg, {"element":"node", "type":"process"});
+        //var get_emiters = graph("edge type=get");
+        //var set_emiters = graph("edge type=set");
+        //var flow_emiters = graph("edge type=flow");
+        //var js_str = '';
+        $.each(listener, function(i, o) {
+            if (o.process) {
+                $("#run_mode_graph_io").on("honeybee " + o.id, function() {
+                    var this_id = "honeybee " + o.id;
+                    var args = get_all(hbg, o.id);
+                    var result = o.process.map(args);
+                    var next_node_id = '';
+                    set_all(this_id, result);
+                    next_node_id = flow_to(this_id, result, this.graph_selector(hbg, {"element":"edge", "type":"flo", "from":o.id});
+                    if (next_node_id) {
+                        emit("honeybee "+next_node_id);
+                    }
+                    else {
+                        emit("honeybee halt");
+                    }
+                });
+            }
+        });
+        $("#run_mode_graph_io").trigger("honeybee start");
+    };
+
+    var graph_selector = function(hbg, sel) {
+        var res = [];
+        if (sel.element === "node") {
+            if (sel.id) {}
+            $.each(hbg.nodes, function(i, o) {
+                if (sel.id && sel.id === o.id) {
+                    res.push(o);
+                }
+                if (sel.type && sel.type === o.node_type) {
+                    res.push(o);
+                }
+            });
+        }
+        else {
+            $.each(hbg.nodes, function(i, o) {
+                if (o[2] === sel.type && o[0] === sel.from) {
+                    res.push(o);
+                }
+            });
+        }
+        return res;
+    };
+    var get_all = function(hbg, id) {
+        var get_arr = [];
+        var get_edges = graph_selector(hbg, {"element":"edge", "type":"get", "from":id});
+        $.each(get_edges, function(i, o) {
+            var end_node = graph_selector(hbg, {"element":"node", "id":id});
+            var name = o.alias | end_node.name;
+            var gotten = {"name":name, "value":end_node.data[name]};
+            get_arr.push(gotten);
+        });
+    };
+    var set_all = function(hbg, id, result) {
+        var set_edges = graph_selector(hbg, {"element":"edge", "type":"set", "from":id});
+        $.each(set_edges, function(i, o) {
+            var end_node = graph_selector(hbg, {"element":"node", "id":id, "ordinal":true});
+            var name = o.alias | end_node.name | "data";
+            hbg.nodes[end_node.ordinal].data[name] = result[name]; 
+        });
+    }
+    
+    
+})();
