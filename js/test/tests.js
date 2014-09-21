b94820f02ad29aa14e9ead41fb56516b27031ad2
Nate Morse <morse.nate@gmail.com>
2014-09-20 21:30:41 -0400

    test the graph query selector (a start anyway)

--- js/test/tests.js
+++ js/test/tests.js
@@ -1,3 +1,32 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+
+test( "Graphel selector tests", function( assert ) {
+  var graph = {"graph":{"name":"Hello World"},"nodes":[
+  {"name":"start","id":"n0","view":{"position":{"x":124,"y":80}},"node_type":"process"},
+  {"name":"send","process":["~salutation = ~salutation . ~name"],"data":{"salutation":"Hi "},"id":"n1","view":{"position":{"x":124,"y":196}},"node_type":"process"},
+  {"name":"Hello","data":{"salutation":"Hello "},"id":"n2","view":{"position":{"x":316,"y":105}},"node_type":"data"},
+  {"name":"out","type":"io","node_type":"io","id":"n3","view":{"position":{"x":446,"y":304}}},
+  {"name":"name","type":"io","subtype":"POST","data":{"name":"World"},"node_type":"io","id":"n4","view":{"position":{"x":446,"y":196}}},
+  {"name":"end","id":"n5","view":{"position":{"x":124,"y":302}},"node_type":"process"}
+ ],
+ "edges":[
+  ["n0","n1","flo","",null],
+  ["n1","n2","get","salutation",null],
+  ["n1","n4","get","name",null],
+  ["n1","n3","set","greeting",null],
+  ["n1","n5","evt","",null]
+ ]
+};
+	
+  assert.deepEqual( 
+	gq.using(graph).find({"element":"node", "id":"n1"}),  
+	[{"name":"send","process":["~salutation = ~salutation . ~name"],"data":{"salutation":"Hi "},"id":"n1","view":{"position":{"x":124,"y":196}},"node_type":"process"}],
+	"selection of node n1" 
+  );
+  assert.deepEqual( 
+	gq.using(graph).find({"element":"edge", "type":"get", "from":"n1"}),  
+  [["n1","n2","get","salutation",null],
+  ["n1","n4","get","name",null]],
+	"get edges out node n1" 
+  );
 });
+
