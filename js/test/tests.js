280bea4e3368b27486a6997a00e3880315b2e0cd
Nate Morse <morse.nate@gmail.com>
2014-09-23 22:42:30 -0400

    new test added for Hello World example graphlet

--- js/test/tests.js
+++ js/test/tests.js
@@ -64,29 +64,35 @@ test( "Graphlet selector tests", function(assert) {
 });
 
 
-test( "Graphlet procedural run test", function(assert) {
-  expect(0);
-  var graph = {"graph":{"name":"Hello World"},"nodes":[
-	  {"name":"start","id":"n0", "io":{"type":"button"},"view":{"position":{"x":124,"y":80}},"node_type":"io"},
-	  {"name":"send","process":["~salutation = ~salutation . ~name"],"data":{"salutation":"Hi "},"id":"n1","view":{"position":{"x":124,"y":196}},"node_type":"process"},
-	  {"name":"Hello","data":{"salutation":"Hello "},"id":"n2","view":{"position":{"x":316,"y":105}},"node_type":"data"},
-	  {"name":"out","node_type":"io","id":"n3","view":{"position":{"x":446,"y":304}}},
-	  {"name":"name","io":{"method":"POST","data":{"name":"World"}},"node_type":"io","id":"n4","view":{"position":{"x":446,"y":196}}},
+test( "Graphlet procedural run test on a simple Hello World graphlet", function(assert) {
+  expect(1);
+  var $env = $("body");
+  var $fixture = $env.append('<div id="graphlet">loading</div>');
+
+  
+  
+  init_graphlet({"graph":{"name":"Hello World", "template":"<button id='start_button'>Say Hello</button><div class='greeting'></div>"},"nodes":[
+	  {"name":"start","id":"n0", "io":{"selector":"#start_button", "event":"click"},"view":{"position":{"x":124,"y":80}},"node_type":"io"},
+	  {"name":"send","process":["this.greeting = salutation + ' ' + name;"],"id":"n1","view":{"position":{"x":124,"y":196}},"node_type":"process"},
+	  {"name":"Hello","data":{"salutation":"Hello"},"id":"n2","view":{"position":{"x":316,"y":105}},"node_type":"data"},
+	  {"name":"greeting","node_type":"io", "io":{"selector":".greeting"},"id":"n3","view":{"position":{"x":446,"y":304}}},
+	  {"name":"name","data":{"name":"World"},"node_type":"data","id":"n4","view":{"position":{"x":446,"y":196}}},
 	  {"name":"end","id":"n5","view":{"position":{"x":124,"y":302}},"node_type":"process"}
 	 ],
 	 "edges":[
-	  ["n0","n1","flo","",null,0],
+	  ["n0","n1","evt","click",null,0],
 	  ["n1","n2","get","salutation",null,1],
 	  ["n1","n4","get","name",null,2],
 	  ["n1","n3","set","greeting",null,3],
-	  ["n1","n5","evt","",null,4]
+	  ["n1","n5","flo","",null,4]
 	 ]
-  };
-	
-  // 1
-  //assert.deepEqual( 
-//	run(graph),  
-//	{"data":{"salutation":"Hello","name":"World"}},
-//	"run the graph" 
-//  );
+  });
+  
+  
+  
+  
+  // trigger event
+  $('#start_button').trigger("click");
+  
+  assert.equal( $('.greeting').text(), "Hello World", "the graphlet ran and set the greeting to Hello World" );
 });
