51b868a8db655da858ec0924951124f539a11aa9
Nate Morse <morse.nate@gmail.com>
2014-10-19 12:16:30 -0400

    look Ma, no views in the core graph

--- js/graphlet_examples.js
+++ js/graphlet_examples.js
@@ -20,12 +20,12 @@ var graph_examples = {
  },
  "edges":{}}},
 "graph":{"name":"Hello World version 1","template":"<button id='start_button'>Say Hello</button><div class='greeting'></div>"}, "nodes":[
-  {"name":"start","id":"n0","io":{"selector":"#start_button"},"view":{"position":{"x":124,"y":80}},"node_type":"io"},
-  {"name":"send","process":["this.greeting = salutation + ' ' + name;"],"id":"n1","view":{"position":{"x":124,"y":196}},"node_type":"process"},
-  {"name":"Hello","data":{"salutation":"Hello"},"id":"n2","view":{"position":{"x":316,"y":105}},"node_type":"data"},
-  {"name":"greeting","node_type":"io","io":{"selector":".greeting"},"id":"n3","view":{"position":{"x":446,"y":302}}},
-  {"name":"name","data":{"name":"World"},"node_type":"data","id":"n4","view":{"position":{"x":446,"y":196}}},
-  {"name":"end","id":"n5","view":{"position":{"x":124,"y":302}},"node_type":"data","data":{"color":"rgb(255, 0, 0)"}}
+  {"name":"start","id":"n0","io":{"selector":"#start_button"},"node_type":"io"},
+  {"name":"send","process":["this.greeting = salutation + ' ' + name;"],"id":"n1","node_type":"process"},
+  {"name":"Hello","data":{"salutation":"Hello"},"id":"n2","node_type":"data"},
+  {"name":"greeting","node_type":"io","io":{"selector":".greeting"},"id":"n3"},
+  {"name":"name","data":{"name":"World"},"node_type":"data","id":"n4"},
+  {"name":"end","id":"n5","node_type":"data","data":{"color":"rgb(255, 0, 0)"}}
  ],
  "edges":[
   ["n0","n1","sub","click",null,0],
