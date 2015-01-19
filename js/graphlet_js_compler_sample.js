6817ee1ff16380fdab0145fbb7d70fc7e5d6975a
Nate Morse <morse.nate@gmail.com>
2015-01-18 23:29:09 -0500

    more working on the target

--- js/graphlet_js_compler_sample.js
+++ js/graphlet_js_compler_sample.js
@@ -1,7 +1,20 @@
 // sample output that the graphlet_js_compiler will generate.
 
 (function($) {
-
+  var g = {"graph":{"name":"counter 1","template":"<button id='start_button'>Start</button><div class='counter'></div>"},
+  "nodes":[
+  {"id":"n3","name":"c","io":{"selector":".counter"},"data":{"c":0}},
+  {"name":"","id":"n5","data":{"c":0}},
+  {"name":"+1","process":[function(in){this.c = in.c + 1;return this;}],"id":"n1"},
+  {"name":"start","id":"n0","io":{"selector":"#start_button"}}
+ ],
+ "edges":[
+  {"to":"n1","type":"msg","name":"click","guard":"","id":0}, // put these in on_init listner hookup
+  {"from":"n1","to":"n5","type":"flo","name":"","guard":function(in) {return (in.c > 5);},"id":1},
+  {"from":"n1","to":"n3","type":"set","name":"","guard":function(in) {return (in.c <= 5);},"id":2},
+  {"from":"n1","to":"n3","type":"get","name":"","guard":"","id":3},
+  {"from":"n5","to":"n3","type":"set","name":"","guard":"","id":4}
+ ]};
   var node_queue = [];
   var debug_mode = "";
   var nodes = {
@@ -44,8 +57,21 @@
     }
   };
 
+  var nodify = function (node) {
+    node.get_all = function() {
+
+    };
+    node.process = function() {
+
+    };
+    return node;
+  };
+
   // interface
   init = function () {
+    // augment nodes objects by adding functionality
+
+
     // init by setting up io listeners
 
     // init io elements from the environment
