00e6fc90e47960932ff58b96aa16c703130779b4
Nate Morse <morse.nate@gmail.com>
2015-05-05 22:20:27 -0400

    clean out nonworking graphs

--- js/graphlet_examples.js
+++ js/graphlet_examples.js
@@ -376,80 +376,8 @@ var graph_examples = {
  ],
  "views":[{"name":"different","nodes":{"n5":{"position":{"x":538,"y":33},"width":70},"n14":{"position":{"x":322,"y":135}},"n15":{"position":{"x":160,"y":293}},"n6":{"position":{"x":71,"y":33},"width":70},"n10":{"position":{"x":303,"y":33},"width":70},"n11":{"position":{"x":78,"y":129},"width":70},"n12":{"position":{"x":501,"y":255},"width":70},"n13":{"position":{"x":365,"y":389},"width":70},"n4":{"position":{"x":259,"y":332},"width":70},"n2":{"position":{"x":410,"y":97},"width":70},"n1":{"position":{"x":419,"y":172},"width":70},"n0":{"position":{"x":225,"y":97},"width":70},"n3":{"position":{"x":246,"y":253},"width":70},"n7":{"position":{"x":60,"y":255},"width":70},"n8":{"position":{"x":225,"y":172},"width":70},"n9":{"position":{"x":60,"y":330},"width":70}},"edges":{}}]
 },
-"Guessing Game 1":
-{"graph":{"name":"Guessing Game 1","template":"<div id='prompt'></div><input id='guess'/><button id='enter_button'>Enter</button>"}, "nodes":[
-  {"id":"n4","name":"prompt","data":{"prompt":"Too High, guess again"}},
-  {"id":"n2","name":"prompt","data":{"prompt":"Too Low, guess again"}},
-  {"id":"n10","name":"graph"},
-  {"name":"enter","id":"n0","io":{"selector":"#enter_button"}},
-  {"name":"think","process":["this.secret = Math.floor(Math.random(1)*100);"],"id":"n1"},
-  {"name":"prompt","io":{"selector":"#prompt"},"id":"n3"},
-  {"name":"prompt","id":"n5","data":{"prompt":"Guess a number (1 - 100)"}},
-  {"id":"n6","name":"secret"},
-  {"id":"n7","name":"diff","process":["this.diff = guess - secret;"]},
-  {"id":"n8","name":"guess","io":{"selector":"#guess","valve":3}},
-  {"id":"n9","name":"prompt","data":{"prompt":"Great! You guessed it."}}
- ],
- "edges":[
-  ["n1","n5","flo","next","",0],
-  ["n1","n6","set","","",1],
-  ["n5","n3","set","","",2],
-  ["n0","n7","sub","click","",3],
-  ["n7","n8","get","","",4],
-  ["n7","n6","get","","",5],
-  ["n7","n9","flo","","diff === 0",6],
-  ["n9","n3","set","","",7],
-  ["n10","n1","sub","graph_init","",8],
-  ["n2","n3","set","","",9],
-  ["n7","n2","flo","","diff < 0",10],
-  ["n4","n3","set","","",11],
-  ["n7","n4","flo","","diff > 0",12]
- ],
- "views":[{"name":"primary","nodes":{"n4":{"position":{"x":247,"y":164},"width":180},"n2":{"position":{"x":287,"y":227},"width":180},"n10":{"position":{"x":5,"y":19},"width":60},"n0":{"position":{"x":526,"y":365},"width":60},"n1":{"position":{"x":69,"y":96},"width":60},"n3":{"position":{"x":526,"y":281},"width":60},"n5":{"position":{"x":428,"y":96},"width":200},"n6":{"position":{"x":69,"y":286},"width":60},"n7":{"position":{"x":198,"y":363},"width":80},"n8":{"position":{"x":526,"y":323},"width":60},"n9":{"position":{"x":351,"y":279},"width":180}},"edges":{}}]
-},
-"Guessing Game 2":
-{"graph":{"name":"Guessing Game 2","template":"<div id='prompt'></div><input id='guess'/><button id='enter_button'>Enter</button>"}, "nodes":[
-  {"id":"n9","name":"prompt","data":{"prompt":"Great! You guessed it, in {{count}} guess(es) "},"process":["this.prompt = prompt.replace(\"{{count}}\", count);"]},
-  {"id":"n8","name":"guess","io":{"selector":"#guess","valve":3}},
-  {"id":"n7","name":"diff","process":["this.diff = guess - secret;"]},
-  {"id":"n6","name":"secret"},
-  {"name":"prompt","id":"n5","data":{"prompt":"Guess a number (1 - 100)"}},
-  {"name":"prompt","io":{"selector":"#prompt"},"id":"n3"},
-  {"name":"think","process":["this.secret = Math.floor(Math.random(1)*100) + 1;"],"id":"n1"},
-  {"name":"enter","id":"n0","io":{"selector":"#enter_button"}},
-  {"id":"n10","name":"env"},
-  {"id":"n2","name":"prompt","data":{"prompt":"Too Low, guess again"}},
-  {"id":"n4","name":"prompt","data":{"prompt":"Too High, guess again"}},
-  {"id":"n11","name":"","process":["this.count = +count + 1;"]},
-  {"id":"n12","name":"count","io":{"selector":"#count","valve":3},"data":{"count":0}},
-  {"id":"n13","name":"prompt","data":{"prompt":"Sorry, all guesses are used up, the number was "},"process":["this.prompt = prompt + secret"]}
- ],
- "edges":[
-  ["n1","n5","flo","next","",0],
-  ["n1","n6","set","","",1],
-  ["n5","n3","set","","",2],
-  ["n7","n8","get","","",3],
-  ["n7","n6","get","","",4],
-  ["n7","n9","flo","","",5],
-  ["n9","n3","set","","",6],
-  ["n10","n1","sub","graph_init","",7],
-  ["n2","n3","set","","",8],
-  ["n7","n2","flo","","diff < 0",9],
-  ["n4","n3","set","","",10],
-  ["n7","n4","flo","","diff > 0",11],
-  ["n0","n11","sub","click","",12],
-  ["n11","n12","get","","",13],
-  ["n11","n12","set","","",14],
-  ["n11","n7","flo","","count < 7",15],
-  ["n11","n13","flo","","",16],
-  ["n13","n3","set","","",17],
-  ["n13","n6","get","","",18],
-  ["n9","n12","get","","",19]
- ],
- "views":[{"name":"primary","nodes":{"n9":{"position":{"x":326,"y":241},"width":180},"n8":{"position":{"x":533,"y":323},"width":60},"n7":{"position":{"x":198,"y":363},"width":80},"n6":{"position":{"x":69,"y":286},"width":60},"n5":{"position":{"x":428,"y":96},"width":200},"n3":{"position":{"x":533,"y":208},"width":60},"n1":{"position":{"x":69,"y":96},"width":60},"n0":{"position":{"x":533,"y":386},"width":60},"n10":{"position":{"x":5,"y":19},"width":60},"n2":{"position":{"x":249,"y":184},"width":180},"n4":{"position":{"x":189,"y":138},"width":180},"n11":{"position":{"x":343,"y":431},"width":60},"n12":{"position":{"x":533,"y":440},"width":60},"n13":{"position":{"x":362,"y":303},"width":60}},"edges":{}}]
-},
-"Guessing Game 3":
-{"graph":{"name":"Guessing Game 3","template":"<div id='prompt'></div><input id='guess'/><button id='enter_button'>Enter</button>"}, "nodes":[
+"Guessing Game":
+{"graph":{"name":"Guessing Game","template":"<div id='prompt'></div><input id='guess'/><button id='enter_button'>Enter</button>"}, "nodes":[
   {"id":"n16","name":"end","process":["this.prompt =  prompt + \", no more guesses left. The secret number of \" + secret;"]},
   {"id":"n15","name":"guess again","process":["this.prompt =  prompt + \", guess again\";"]},
   {"id":"n14","name":"game over?","process":["this.game_over =  (count>=7);"]},
